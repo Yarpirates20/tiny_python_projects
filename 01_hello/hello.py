d516aba8ac8438ed7b9cbfd5dc53cd62ed880f42
Rob <rsamoraj11@gmail.com>
2022-11-01 11:46:42 -0500

    Add documentation with yapf for clarity
    Use yapf in VS Code with ALT + CTRL + F to format.
    
    Meet the standards of flake8 and pylint. Create documentation and doc-strings in required areas, and add in necessary information.

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,17 +1,30 @@
 #! /usr/bin/env python3
-# Say hello
+"""
+Author: Robert Samoraj
+Purpose: Say hello
+"""
 
 import argparse
 
+
 def get_args():
+    """ Get the command-lind arguments """
+
     parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
+    parser.add_argument('-n',
+                        '--name',
+                        metavar='name',
+                        default='World',
+                        help='Name to greet')
     return parser.parse_args()
 
+
 def main():
+    """ main """
     args = get_args()
     name = args.name
     print('Hello, ' + name + '!')
+
 
 if __name__ == '__main__':
     main()
