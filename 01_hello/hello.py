82e8e35efb81aa9233bd93f17d2e13303964e0c5
Rob <rsamoraj11@gmail.com>
2022-11-01 11:28:47 -0500

    Refactor hello.py to learn
    Move code parsing arguments into separate function.
    
    Keep the main function short and uncluttered for readability and learning. Call get_args() function in main() to run the argument parser.

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -3,8 +3,15 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
-args = parser.parse_args()
-name = args.name
-print('Hello, ' + name + '!')
+def get_args():
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    name = args.name
+    print('Hello, ' + name + '!')
+
+if __name__ == '__main__':
+    main()
