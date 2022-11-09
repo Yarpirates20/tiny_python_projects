25b8efe11f8b597e16689765e58866c9c9762408
Rob <rsamoraj11@gmail.com>
2022-11-08 19:14:18 -0600

    Put in conditional statements and join strings to output list

--- 03_picnic/picnic.py
+++ 03_picnic/picnic.py
@@ -2,7 +2,7 @@
 """
 Author : Rob Samoraj <snailfail@vivaldi.net>
 Date   : 2022-11-07
-Purpose: Output formatted list
+Purpose: Print items on picnic
 """
 
 import argparse
@@ -13,11 +13,12 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Output list',
+        description='Print items on picnic',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('items',
-                        metavar='str',
+                        metavar='item',
+                        type=str,
                         nargs='+',
                         help='Item(s) to bring')
 
@@ -35,8 +36,16 @@ def main():
     """Output formatted list of items to bring to picnic"""
 
     args = get_args()
-    print(args.items)
+    items = args.items
+    # print(args.items)
+    # print(args.sorted)
 
+    if len(items) == 1:
+        print(f'You are bringing {items[0]}.')
+    elif len(items) == 2:
+        print(f'You are bringing {items[0]} and {items[1]}.')
+    else:
+        print('You are bringing ' + ', '.join(items[:-1]) + ', and ' + items[-1] + '.')
 
 # --------------------------------------------------
 if __name__ == '__main__':
