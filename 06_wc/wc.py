ff0b8e962282dcb8e5625d5d12ac8b1b7d9e5aa8
Rob <rsamoraj11@gmail.com>
2022-11-19 21:35:24 -0600

    Create flag options to count elements
    Use single letter flags in arguments to count lines, words, or characters, displaying each specified item. If any of the flags are present, in main(), print the count of the flagged variable, adding an if statement on the end to only print it if the flag is specified. Else print an empty string, consistent with the default/main parts of the program.

--- 06_wc/wc.py
+++ 06_wc/wc.py
@@ -8,20 +8,45 @@ Purpose: Word count emulator
 import argparse
 import sys
 
+
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Word count emulator',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Word count emulator",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('file',
-                        help='Input files',
-                        metavar='FILE',
-                        nargs='*',
-                        type=argparse.FileType('r'),
-                        default=[sys.stdin])
+    parser.add_argument(
+        "file",
+        help="Input files",
+        metavar="FILE",
+        nargs="*",  # 0 or more
+        type=argparse.FileType("r"),
+        default=[sys.stdin],  # Allows piping this program into cli
+    )
+
+    parser.add_argument(
+        "-l",
+        "--line",
+        help="Line count",
+        action='store_true'
+    )
+
+    parser.add_argument(
+        "-w",
+        "--word",
+        help="Word count",
+        action='store_true'
+    )
+
+    parser.add_argument(
+        "-c",
+        "--chars",
+        help="Byte/character count",
+        action='store_true'
+    )        
 
     return parser.parse_args()
 
@@ -40,15 +65,23 @@ def main():
             num_lines += 1
             num_words += len(line.split())
             num_bytes += len(line)
-            
-        print(f'{num_lines:8}{num_words:8}{num_bytes:8} {fh.name}')
+
+        if args.line or args.word or args.chars:
+            print(f'{num_lines:8}' if args.line else '', end='')
+            print(f'{num_words:8}' if args.word else '', end='')
+            print(f'{num_bytes:8}' if args.chars else '', end='')
+            print(f' {fh.name}')
+        else:
+            print(f"{num_lines:8}{num_words:8}{num_bytes:8} {fh.name}")
+        
         total_lines += num_lines
         total_words += num_words
         total_bytes += num_bytes
-    
+
     if len(args.file) > 1:
-        print(f'{total_lines:8}{total_words:8}{total_bytes:8} total')
+        print(f"{total_lines:8}{total_words:8}{total_bytes:8} total")
+
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
