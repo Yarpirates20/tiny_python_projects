a7132c98e9bc02e184b1469c64531fd357ddb29c
Rob <rsamoraj11@gmail.com>
2022-11-20 19:49:16 -0600

    Initial commit
    Added default text to file argument in argparse.
    
    Iterate over each line in file handle(args.file), creating a key value pair of the text, and adding them to lookup(dict).
    
    Iterate over each letter argument, print the line that begins with the input after checking lookup, and if not found insert default value with formatted string.

---
+++ 07_gashlycrumb/gashlycrumb.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Author : Rob Samoraj <snailfail@vivaldi.net>
+Date   : 2022-11-20
+Purpose: Prints line from a file starting with given letter
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Prints line from a file starting with given letter',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('letters',
+                        metavar='letter',
+                        nargs='+',
+                        help='Letters')
+
+    parser.add_argument('-f',
+                        '--file',
+                        help='Input file',
+                        metavar='FILE',
+                        type=argparse.FileType('r'),
+                        default='gashlycrumb.txt')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    lookup = {}
+    for line in args.file:
+        key = line[0].upper()   
+        value = line[:-1]
+        lookup[key] = value
+    
+    for letter in args.letters:
+        print(lookup.get(letter.upper(), f'I do not know "{letter}".'))
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
