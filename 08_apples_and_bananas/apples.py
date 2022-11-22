4b55025549f97707a52c54afed87e42d9988242b
Rob <rsamoraj11@gmail.com>
2022-11-22 11:36:38 -0600

    Use nested loops to replace vowels
    Use branching statements to loop through each letter, see if it is a vowel, and replace it with the matching upper or lower case input vowel.

--- 08_apples_and_bananas/apples.py
+++ 08_apples_and_bananas/apples.py
@@ -45,14 +45,29 @@ def main():
 
     args = get_args()
 
-    reg_ex = re.compile('[aeiou]')
     text = args.text
     vowel = args.vowel
-    m = reg_ex.match(vowel)
+    replaced = ''
 
-    if m:
-        replaced = reg_ex.sub(vowel, text)
-        print(replaced)
+    for char in text:
+        if char in 'aeiouAEIOU':
+            if char.isupper():
+                replaced += vowel.upper()
+            elif char.islower():
+                replaced += vowel.lower()
+        else:
+                replaced += char
+        
+    print(f'{replaced}\n', end='')
+
+    # reg_ex = re.compile('[aeiou]', re.IGNORECASE)
+    # text = args.text
+    # vowel = args.vowel
+    # m = reg_ex.match(vowel)
+
+    # if m:
+    #     replaced = reg_ex.sub(vowel, text)
+    #     print(replaced)
 
 
 
