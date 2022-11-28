00ea9971005dc2ec4eb22a3e7b0f0172a6fbd4b3
Rob <rsamoraj11@gmail.com>
2022-11-28 11:14:04 -0600

    Init commit
    Write choose() to return upper if False, lower if True. Unit test to accompany choose().
    
    For loop to go through each line of args.text and run the choose function, adding the returned value to new_text which is printed.

---
+++ 12_ransom/ransom.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+Author : Rob Samoraj <snailfail@vivaldi.net>
+Date   : 2022-11-28
+Purpose: Create a ransom note
+"""
+
+import argparse
+import os
+import random
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Create a ransom note',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text',
+                        metavar='str',
+                        help='Input text or file')
+
+    parser.add_argument('-s',
+                        '--seed',
+                        help='Random seed value',
+                        metavar='seed',
+                        type=int,
+                        default=None)
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.text):
+        args.text = open(args.text).read().rstrip()
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    random.seed(args.seed)
+    new_text = ''
+
+    for char in args.text:
+        new_text += choose(char)
+    
+    print(new_text)
+
+
+# --------------------------------------------------
+def choose(char):
+    """Return an upper or lower case char"""
+
+    return char.upper() if random.choice([False, True]) else char.lower()
+
+
+# --------------------------------------------------
+def test_choose():
+    """Test choose()"""
+    state = random.getstate()
+    random.seed(1)
+    assert choose('a') == 'a'
+    assert choose('b') == 'b'
+    assert choose('c') == 'C'
+    assert choose('d') == 'd'
+    random.setstate(state)
+    
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
