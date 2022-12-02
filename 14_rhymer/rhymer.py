75926fd6076765c1a64fffce7dacc383e846970c
Rob <rsamoraj11@gmail.com>
2022-12-01 18:37:07 -0600

    Create function to break word
    Stemmer() takes word input from args.word and returns the word in two parts. If the word begins with a consonant, the function returns the consonant(s) and the rest of the word.
    
    If the word begins with a vowel, the function returns the word in the form of a tuple with an empty string as the first element. The second element is the word. If the word has no vowels, the function returns the word in the form of a tuple with the word as the first element and an empty string as the second element.

--- 14_rhymer/rhymer.py
+++ 14_rhymer/rhymer.py
@@ -8,7 +8,9 @@ Purpose: Make rhyming words
 import argparse
 import os
 import re
+import string
 import sys
+
 
 
 # --------------------------------------------------
@@ -19,9 +21,7 @@ def get_args():
         description='Make rhyming words',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('word',
-                        metavar='word',
-                        help='A word to rhyme')
+    parser.add_argument('word', metavar='word', help='A word to rhyme')
 
     return parser.parse_args()
 
@@ -31,8 +31,33 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    print(args.word)
-    
+    print(stemmer(args.word))
+
+
+# --------------------------------------------------
+def stemmer(word):
+    """Break word into consonants and the rest"""
+    consonants = ''.join([c for c in string.ascii_lowercase if c not in 'aeiou'])
+    match = re.match(f'([{consonants}]+)?([aeiou])(.*)', word.lower())
+    if match:
+        p1 = match.group(1) or ''
+        p2 = match.group(2) or ''
+        p3 = match.group(3) or ''
+        return (p1, p2 + p3)
+    else:
+        return (word.lower(), '')
+
+# --------------------------------------------------
+def test_stemmer():
+    """ Test stemmer """
+
+    assert stemmer('') == ('', '')
+    assert stemmer('cake') == ('c', 'ake')
+    assert stemmer('chair') == ('ch', 'air')
+    assert stemmer('APPLE') == ('', 'apple')
+    assert stemmer('RDNZL') == ('rdnzl', '')
+    assert stemmer('123') == ('123', '')
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
