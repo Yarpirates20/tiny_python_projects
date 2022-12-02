c5c4ee7c05d92d47feed619be996e4b615a2bc8d
Rob <rsamoraj11@gmail.com>
2022-12-02 09:40:18 -0600

    Format regular expression with re.VERBOSE flag for readability

--- 14_rhymer/rhymer.py
+++ 14_rhymer/rhymer.py
@@ -6,11 +6,8 @@ Purpose: Make rhyming words
 """
 
 import argparse
-import os
 import re
 import string
-import sys
-
 
 
 # --------------------------------------------------
@@ -18,10 +15,11 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Make rhyming words',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Make rhyming words",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('word', metavar='word', help='A word to rhyme')
+    parser.add_argument("word", metavar="word", help="A word to rhyme")
 
     return parser.parse_args()
 
@@ -35,12 +33,14 @@ def main():
     # consonants = [c for c in string.ascii_lowercase if c not in 'aeiou']
     # clusters = ['bl', 'br', 'ch', 'cl', 'cr', 'dr', 'fl', 'fr', 'gl', 'gr', 'pl', 'pr', 'sc', 'sh', 'sk', 'sl', 'sm', 'sn', 'sp', 'st', 'sw', 'th', 'tr', 'tw', 'thw', 'wh', 'wr', 'sch', 'scr', 'shr', 'sph', 'spl', 'spr', 'squ', 'str', 'thr']
 
-    prefixes = list('bcdfghjklmnpqrstvwxyz') + ('bl br ch cl cr dr fl fr gl gr pl pr sc sh sk sl sm sn sp st sw th tr tw thw wh wr sch scr shr sph spl spr squ str thr').split()
+    prefixes = (list("bcdfghjklmnpqrstvwxyz") + (
+        "bl br ch cl cr dr fl fr gl gr pl pr sc sh sk sl sm sn sp st sw th tr tw thw wh wr sch scr shr sph spl spr squ str thr"
+    ).split())
 
     start, rest = stemmer(args.word)
 
     if rest:
-        words = '\n'.join(sorted([p + rest for p in prefixes if p != start]))
+        words = "\n".join(sorted([p + rest for p in prefixes if p != start]))
         print(words)
     else:
         print(f'Cannot rhyme "{args.word}"')
@@ -48,10 +48,9 @@ def main():
     # letters = consonants + clusters
     # rhymes = []
     # s1, s2 = stemmer(word.lower())
-    
 
     # rhymes = (i+s2 for i in letters if i != s1)
-   
+
     # print('\n'.join(sorted(rhymes)))  if 'aeiou' in s1 or s2 else print(f'Cannot rhyme "{args.word}"')
     # print(f'{clusters[0]}{s2}')
 
@@ -59,28 +58,34 @@ def main():
 # --------------------------------------------------
 def stemmer(word):
     """Break word into consonants and the rest"""
-    consonants = ''.join([c for c in string.ascii_lowercase if c not in 'aeiou'])
-    match = re.match(f'([{consonants}]+)?([aeiou])(.*)', word.lower())
+    consonants = "".join(
+        [c for c in string.ascii_lowercase if c not in "aeiou"])
+    match = re.match(f"""
+        ([{consonants}]+)?                      # capture one or more consonants, optional          
+        ([aeiou])                               # capture at least one vowel
+        (.*)                                    # zero or more of anything else
+        """, word.lower(), re.VERBOSE)          # end capture
     if match:
-        p1 = match.group(1) or ''
-        p2 = match.group(2) or ''
-        p3 = match.group(3) or ''
+        p1 = match.group(1) or ""
+        p2 = match.group(2) or ""
+        p3 = match.group(3) or ""
         return (p1, p2 + p3)
     else:
-        return (word.lower(), '')
+        return (word.lower(), "")
+
 
 # --------------------------------------------------
 def test_stemmer():
-    """ Test stemmer """
+    """Test stemmer"""
 
-    assert stemmer('') == ('', '')
-    assert stemmer('cake') == ('c', 'ake')
-    assert stemmer('chair') == ('ch', 'air')
-    assert stemmer('APPLE') == ('', 'apple')
-    assert stemmer('RDNZL') == ('rdnzl', '')
-    assert stemmer('123') == ('123', '')
+    assert stemmer("") == ("", "")
+    assert stemmer("cake") == ("c", "ake")
+    assert stemmer("chair") == ("ch", "air")
+    assert stemmer("APPLE") == ("", "apple")
+    assert stemmer("RDNZL") == ("rdnzl", "")
+    assert stemmer("123") == ("123", "")
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
