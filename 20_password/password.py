3ef40a709c48089ba413ca5e60a669903e6838ea
Rob <rsamoraj11@gmail.com>
2022-12-28 19:29:16 -0600

    Refactor for readability

--- 20_password/password.py
+++ 20_password/password.py
@@ -89,29 +89,37 @@ def main():
     passwords = [''.join(random.sample(words, k=args.num_words)) for _ in range(args.num)]
 
     if args.l33t:
-        encoded = map(l33t, passwords)
-        print('\n'.join(encoded))
+        passwords = map(l33t, passwords)
+        # print('\n'.join(encoded))
     # print(''.join(random.sample(words, k=args.num)))
-    else:
-        print('\n'.join(passwords))
+
+    print('\n'.join(passwords))
 
 # --------------------------------------------------
 def clean(word):
     """Remove non-letters from words"""
 
-    sub = re.sub(r"[^A-Za-z]", '', word)
-    return sub
+    return re.sub(r"[^A-Za-z]", '', word)
 
 
 #--------------------------------------------------
 def ransom(word):
+    """Randomly uppercase letters in a word"""
+
+    ### MY SOLUTION ###
     ransom_word = [x.upper() if random.choice([False, True]) else x.lower() for x in word]
 
     return ''.join(ransom_word)
 
+    ### BOOK SOLUTION ###
+    # return ''.join(
+        # map(lambda c: c.upper() if random.choice([0, 1]) else c.lower(), word)
+    # )
+
 
 # --------------------------------------------------
 def l33t(text):
+    """Encode text with l33t code"""
     
     l33t_code = {
         'a': '@',
@@ -122,6 +130,23 @@ def l33t(text):
         'I': '1',
         'S': '5'
     }
+
+    ### BOOK SOLUTION ###
+    # text = ransom(text)
+    # xform = str.maketrans({
+    #     'a': '@',
+    #     'A': '4',
+    #     'O': '0',
+    #     't': '+',
+    #     'E': '3',
+    #     'I': '1',
+    #     'S': '5'
+    # })
+
+    # return text.translate(xform) + random.choice(string.punctuation)
+
+
+    ### MY SOLUTION AND ATTEMPTS ###
     # leet_speak = []
     # for char in text:
     #     ransom(char)
@@ -131,11 +156,13 @@ def l33t(text):
     # leet_speak.append(random.choice(string.punctuation))
 
     # return ''.join(leet_speak)
+    ### MY FINAL SOLUTION TO END ###
     ransomed_text = ransom(text)
     leet_speak = [l33t_code.get(char, char) for char in ransomed_text]
     leet_speak.append(random.choice(string.punctuation))
 
     return ''.join(leet_speak)
+    ### END ###
     # punctuation = random.choice(string.punctuation)
     # leet_speak.append(punctuation)
 
